041c940eb586db229e49fc815617d1027a8d7213
kwan <11892929@qq.com>
2020-07-01 18:26:48 +0800

    e

---
+++ src/components/navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="box">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "navbar"
+    }
+</script>
+
+<style scoped>
+.box{
+    height: 49px;
+    position: fixed;
+}
+</style>
